0da9af764d4da25bf006455f29fbb66fc74bd8f8
George Carder <georgercarder@gmail.com>
2018-02-05 17:51:29 -0800

    installed axios. was able to make axios request and display status as this.state.puzzles

---
+++ src/fetchgames.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.css';
+
+function Fetchgames(props){
+	    return(
+					<div><h1>Matrix of games</h1></div>
+				        );
+}
+
+export default Fetchgames;
--- src/site.js
+++ src/site.js
@@ -2,15 +2,22 @@ import React from 'react';
 import './index.css';
 import Game from './game';
 import Introd from './introd';
+import Fetchgames from './fetchgames';
+import axios from 'axios';
 
 class Site extends React.Component {
   
   constructor(props){
     super(props);
     this.state={
-			page: <Introd changepage={(i) => this.changepage(i)}/> 
+			page: <Introd onload={this.handleFetch()} changepage={(i) => this.changepage(i)}/>,
+			puzzles: 'url(path to node)' 
     };
   }
+
+	handleFetch(){
+		axios.get('http://maps.googleapis.com/maps/api/geocode/json?address=chicago').then((response) => this.setState({puzzles: response.status}))
+	}
 
 	changepage(i){
 		if (i===1){
@@ -23,6 +30,7 @@ class Site extends React.Component {
     return (
       <body>
 				{this.state.page}
+				{this.state.puzzles}
       </body>
     );
   }
